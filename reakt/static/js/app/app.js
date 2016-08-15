38cfaf7d9fef48fedb5934be78f1793ddac0b563
Vasiliy Zemlyanov <angeldaren@gmail.com>
2016-08-15 22:55:41 +0300

    Update bokeh tests

--- reakt/static/js/app/app.js
+++ reakt/static/js/app/app.js
@@ -1,13 +1,21 @@
 'use strict';
 
 $(function() {
-	console.log('hello, world!');
+	// console.log('hello, world!');
 
-	var mySpan = React.createElement('span', {
-		style: { fontFamily: 'sans-serif' }}, 'hello React world');
-	var myDiv = React.createElement('div', {
-		className: 'my-div' }, mySpan);
+	// var mySpan = React.createElement('span', {
+	// 	style: { fontFamily: 'sans-serif' }}, 'hello React world');
+	// var myDiv = React.createElement('div', {
+	// 	className: 'my-div' }, mySpan);
 
-	ReactDOM.render(myDiv,
-		document.querySelector('body'));
+	// ReactDOM.render(myDiv,
+	// 	document.querySelector('body'));
+
+	var div = React.createElement('div', {
+		height: 300,
+		width: 300
+	}, [React.createElement('path', {
+		'd': 'M0,0V300H300',
+		style: { fill: 'none', stroke: '#222' }
+	}), ])
 });
